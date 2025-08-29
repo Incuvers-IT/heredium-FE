bc66241118c29db9184b69baef6031d4a92e06c9
kang <khsok212@gmail.com>
2025-08-29 15:28:30 +0900

    앱 기존회원 팝업 호출

--- components/user/modal/NotificationModal.vue
+++ components/user/modal/NotificationModal.vue
@@ -77,7 +77,7 @@ export default {
     async skipMarketing() {
       try {
         // 마케팅 동의 안 함(0)
-        await this.$axios.$put('/user/account/marketing', {
+        const res = await this.$axios.$put('/user/account/marketing', {
           marketingPending: false,
           isMarketingReceive: false,
           isLocalResident: false,
@@ -85,6 +85,7 @@ export default {
           issueMembershipCoupons: true,
         });
 
+        this.$store.commit('service/auth/setUserInfo', res);
       } catch (err) {
         console.warn('마케팅 스킵 처리 실패', err);
       } finally {
